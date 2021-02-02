3d4644f6b5cd83d0fdb7cd6f7bedac90d202413e
Farah <21901299@unicaen.fr>
2021-02-02 17:23:04 +0100

    Fonction toggleActive() - Fin Route ex2

--- app/routes/ex2.js
+++ app/routes/ex2.js
@@ -1,9 +1,20 @@
 import Route from '@ember/routing/route';
 import Services from 'td1/classes/services';
 import { datas } from 'td1/data/datas';
+import { action } from '@ember/object';
 
 export default class Ex2Route extends Route {
   model(){
     return new Services(datas);
+  }
+
+  @action
+  toggleActive(service){
+    if(service.active){
+      service = !service.active;
+    }
+    else {
+      service.active;
+    }
   }
 }
