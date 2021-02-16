2522ebe5101cc31e2ea2301db8455783e4e6a2ec
Farah <21901299@unicaen.fr>
2021-02-16 15:22:26 +0100

    Création Model --> OK

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,4 +1,4 @@
-import Adapter from '@ember-local-storage/adapters/local';
+import Adapter from 'ember-local-storage/adapters/local';
 
 export default Adapter.extend({
   modelNamespace: 'contact-app'
