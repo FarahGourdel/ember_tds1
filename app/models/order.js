160d2c7090f67f772e92861e7393e3fef9d1dfb5
Farah <21901299@unicaen.fr>
2021-03-24 14:19:13 +0100

    Model et serializer 'product'

--- app/models/order.js
+++ app/models/order.js
@@ -3,7 +3,7 @@ import Model, { attr, hasMany } from '@ember-data/model';
 export default class OrderModel extends Model {
   @attr('date') dateCreation;
   @hasMany('orderdetail') orderdetails;
-  get count(){
+  get count() {
     return this.orderdetails.length;
   }
 }
